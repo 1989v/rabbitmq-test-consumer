33f513e4c8b118987b112ed53953795392635246
kdkwon <kdkwon@eduwill.net>
2022-06-23 10:01:20 +0900

    init

---
+++ src/main/java/kr/got/rabbitmqtestconsumer/RabbitmqTestConsumerApplication.java
@@ -0,0 +1,13 @@
+package kr.got.rabbitmqtestconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqTestConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqTestConsumerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/got/rabbitmqtestconsumer/RabbitmqTestConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.got.rabbitmqtestconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqTestConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
